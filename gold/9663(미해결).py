d7b914163b68acd3643efd42fd9aa48b0dc9f59d
rhddnjs1974 <cjw07@naver.com>
2024-03-05 12:46:21 +0900

    1 solve

--- gold/9663(미해결).py
+++ gold/9663(미해결).py
@@ -3,54 +3,33 @@ arr = [[0]*N for i in range(N)]
 
 ans = 0
 
-def bt(n,a,b):
+def bt(n,a):
     global N
     global ans
     if n==0:
         ans+=1
         return
 
-    for i in range(a,N):
-        for j in range(N):
-            if i==a and j<b:
-                continue
+    i = a+1
+    for j in range(N):
+        flag = 0
+        for k in range(1,a+2):
+            if i-k>=0 and j+k<N and arr[i-k][j+k] == 1:
+                flag= 1
+                break
+            if i-k>=0 and j-k>=0 and arr[i-k][j-k] == 1:
+                flag= 1
+                break
+            if i-k>=0 and arr[i-k][j] == 1:
+                flag= 1
+                break
 
-            if arr[i][j] == 0:
-                flag = 0
-                for k in range(1,N):
-                    if i+k<N and j+k<N and arr[i+k][j+k] == 1:
-                        flag= 1
-                        break
-                    if i+k<N and j-k>=0 and arr[i+k][j-k] == 1:
-                        flag= 1
-                        break
-                    if i-k>=0 and j+k<N and arr[i-k][j+k] == 1:
-                        flag= 1
-                        break
-                    if i-k>=0 and j-k>=0 and arr[i-k][j-k] == 1:
-                        flag= 1
-                        break
-                    if i+k<N and arr[i+k][j] == 1:
-                        flag= 1
-                        break
-                    if j+k<N and arr[i][j+k] == 1:
-                        flag = 1
-                        break
-                    if i-k>=0 and arr[i-k][j] == 1:
-                        flag= 1
-                        break
-                    if j-k>=0 and arr[i][j-k] == 1:
-                        flag = 1
-                        break
-
-
-                if flag==0:
-                    arr[i][j] = 1
-                    bt(n-1,i,j)
-                    arr[i][j] = 0
+        if flag==0:
+            arr[i][j] = 1
+            bt(n-1,i)
+            arr[i][j] = 0
 
     return
 
-
-bt(N,0,0)
+bt(N,-1)
 print(ans)
