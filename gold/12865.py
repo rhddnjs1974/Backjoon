1bd2ebfd30c1e24efa6d0901eefbef67eb3ba867
rhddnjs1974 <cjw07@naver.com>
2024-03-09 00:00:04 +0900

    4 solve

--- gold/12865.py
+++ gold/12865.py
@@ -1,15 +1,19 @@
 N,K = map(int,input().split())
 
 arr=[]
-dp = [1e5] * (200001)
+dp = [1e6] * (200001)
+dp[0] = 0
 
 for i in range(N):
     W,V = map(int,input().split())
-    dp[V] = min(dp[V],W)
-    for i in range(100001):
-        dp[i+V] = min(dp[i+V],dp[i]+W)
+    for i in range(100001,-1,-1):
+        if i-V<0:
+            break
+        dp[i] = min(dp[i],dp[i-V]+W)
 
 ma = 0
+
+
 for i in range(100001):
     if dp[i]<=K:
         ma = i
