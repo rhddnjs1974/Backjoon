57c45238549299db1c344288a1972dbd2f351fd1
rhddnjs1974 <cjw07@naver.com>
2024-04-02 12:40:18 +0900

    5 solve / 1 solved again

---
+++ bronze/0xxxx/2587.py
@@ -0,0 +1,7 @@
+arr = []
+for i in range(5):
+    arr.append(int(input()))
+
+print(sum(arr)//5)
+arr.sort()
+print(arr[2])
--- silver/1xxxx/10814.py
+++ silver/1xxxx/10814.py
@@ -7,6 +7,6 @@ for i in range(n):
     a,b = input().split()
     arr.append((a,b))
 
-arr.sort(key=lambda x : (x[0]))
+arr.sort(key=lambda x : (int(x[0])))
 for i,j in arr:
     print(i,j)
