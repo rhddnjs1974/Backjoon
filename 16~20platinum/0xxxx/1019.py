c987e4ef7130758a6bc5444d6235a709c16f222e
rhddnjs1974 <cjw07@naver.com>
2026-01-07 18:30:39 +0900

    push 2026-01-07 18:30:39.42

--- 16~20platinum/0xxxx/1019.py
+++ 16~20platinum/0xxxx/1019.py
@@ -1,3 +1,24 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+arr = [0]*10
+add = 0
+
+i = 1
+while(n!=0):
+    cur = n%10
+    n //= 10
+    
+    arr[0] -= i
+    for j in range(cur):
+        arr[j] += (n+1)*i
+    arr[cur] += (n*i+1+add)
+    for j in range(cur+1,10):
+        arr[j] += n*i
+    
+    add += cur*i
+    
+    i*=10
+
+print(*arr)
